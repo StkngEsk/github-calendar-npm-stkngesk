584b6ae5f4909e565752d949b7be4fcdcc371318
Christian Mena <christian.91.gm@hotmail.com>
2021-05-08 21:32:02 -0500

    Getting github calendar by user and year

---
+++ src/index.js
@@ -0,0 +1,29 @@
+
+const cheerio = require('cheerio');
+const axios = require('axios');
+
+
+const githubCalendar = async (user = "", year = "") => {
+
+    let calendar = "";
+    let textContributions = "";
+    let url = "";
+
+    if (user != "" && year != "") {
+        url = `https://github.com/users/${user}/contributions?from=${year}-01-01&to=${year}-12-31`;
+
+        const { data } = await axios.get(url);
+        const $ = cheerio.load(data);
+        $('div .graph-canvas').each(function () {
+            calendar = $(this).html();
+        });
+
+        $('h2.f4').each(function () {
+            textContributions = $(this).text();
+        });
+    }
+
+    return { textContributions, calendar };
+};
+
+export default githubCalendar;
---
+++ src/index.test.js
@@ -0,0 +1,36 @@
+import githubCalendar from ".";
+const axios = require('axios');
+
+describe("index test ", () => {
+
+    let html = "";
+    let url = "";
+    let data = {};
+
+    beforeEach(() => {
+        html = `
+        <div>
+            <h2 class="f4 text-normal mb-2">Titulo</h2>
+            <div class="graph-canvas">test html</div>
+        </div>
+    `;
+        url = 'https://github.com/users/christianesk/contributions?from=2020-01-01&to=2020-12-31';
+
+        data = {
+            calendar: "test html",
+            textContributions: "Titulo"
+        };
+    });
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    it('should get github calendar', async () => {
+        const getSpy = jest.spyOn(axios, 'get').mockResolvedValueOnce({ data: html });
+        const actual = await githubCalendar('christianesk', "2020");
+        expect(actual).toEqual(data);
+        expect(getSpy).toBeCalledWith(url);
+    });
+
+});
